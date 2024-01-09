edb0df220de59988a8076b7ad3c72df2a3dd9cca
sm-jeon <137497948+sm-jeon@users.noreply.github.com>
2024-01-10 02:48:59 +0900

    [add] 숫자 타자 대회

---
+++ src/main/kotlin/숫자_타자_대회/Solution.kt
@@ -0,0 +1,46 @@
+package 숫자_타자_대회
+
+import kotlin.math.min
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/136797
+
+val cost = arrayOf(
+    intArrayOf(1,7,6,7,5,4,5,3,2,3,),
+    intArrayOf(7,1,2,4,2,3,5,4,5,6,),
+    intArrayOf(6,2,1,2,3,2,3,5,4,5,),
+    intArrayOf(7,4,2,1,5,3,2,6,5,4,),
+    intArrayOf(5,2,3,5,1,2,4,2,3,5,),
+    intArrayOf(4,3,2,3,2,1,2,3,2,3,),
+    intArrayOf(5,5,3,2,4,2,1,5,3,2,),
+    intArrayOf(3,4,5,6,2,3,5,1,2,4,),
+    intArrayOf(2,5,4,5,3,2,3,2,1,2,),
+    intArrayOf(3,6,5,4,5,3,2,4,2,1,),
+)
+
+fun solution(numbers: String): Int {
+    val memo = Array(numbers.length+1) { idx ->
+        Array(10) {
+            IntArray(10) { Int.MAX_VALUE }
+        }
+    }
+
+    memo[0][4][6] = 0
+
+    for(i in 1 .. numbers.length) {
+        var target = numbers[i-1].digitToInt()
+        for(l in 0..9) {
+            for(r in 0..9) {
+                if(memo[i-1][l][r] == Int.MAX_VALUE) continue
+                if(target != r) memo[i][target][r] = min(memo[i][target][r], memo[i-1][l][r] + cost[l][target])
+                if(target != l) memo[i][l][target] = min(memo[i][l][target], memo[i-1][l][r] + cost[r][target])
+            }
+        }
+    }
+    return memo.last().minOf { it.minOf { it } }
+}
+
+class InputCase {
+    companion object {
+        val numbers = "5123"
+    }
+}
